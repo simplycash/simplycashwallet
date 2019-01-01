20e4fc5a2ce6b113d6bf2ddad09b6d1757d139ad
unknown <simplycashdev@gmail.com>
2019-01-02 04:32:53 +0800

    display up to current address

--- src/pages/addresses/addresses.ts
+++ src/pages/addresses/addresses.ts
@@ -10,8 +10,6 @@ import { Wallet } from '../../providers/providers'
 })
 export class AddressesPage {
 
-  public currentReceiveAddr: string
-  public currentChangeAddr: string
   public receiveAddrs: { address: string, path: number[] }[]
   public changeAddrs: { address: string, path: number[] }[]
   public unspentAddrs: { address: string, balance: string, path: number[] }[]
@@ -31,15 +29,17 @@ export class AddressesPage {
       content: this.translate.instant('LOADING_ADDRESSES')+'...'
     })
     await loader.present()
-    this.currentReceiveAddr = this.wallet.getCacheReceiveAddress()
-    this.currentChangeAddr = this.wallet.getCacheChangeAddress()
-    this.receiveAddrs = this.wallet.getAllReceiveAddresses().map((addr: string, i: number, arr: string[]) => {
+    let ara: string[] = this.wallet.getAllReceiveAddresses()
+    ara.length = ara.lastIndexOf(this.wallet.getCacheReceiveAddress()) + 1
+    this.receiveAddrs = ara.map((addr: string, i: number, arr: string[]) => {
       return {
         address: addr,
         path: [0, i]
       }
     }).reverse()
-    this.changeAddrs = this.wallet.getAllChangeAddresses().map((addr: string, i: number, arr: string[]) => {
+    let aca: string[] = this.wallet.getAllChangeAddresses()
+    aca.length = aca.lastIndexOf(this.wallet.getCacheChangeAddress()) + 1
+    this.changeAddrs = aca.map((addr: string, i: number, arr: string[]) => {
       return {
         address: addr,
         path: [1, i]
@@ -52,19 +52,6 @@ export class AddressesPage {
         path: utxo.path
       }
     })
-    // if (this.wallet.getPreferredAddressFormat() === 'cashaddr') {
-    //   this.currentReceiveAddr = this.wallet.convertAddress('legacy', 'cashaddr', this.currentReceiveAddr)
-    //   this.currentChangeAddr = this.wallet.convertAddress('legacy', 'cashaddr', this.currentChangeAddr)
-    //   this.receiveAddrs.forEach((item: any) => {
-    //     item.address = this.wallet.convertAddress('legacy', 'cashaddr', item.address)
-    //   })
-    //   this.changeAddrs.forEach((item: any) => {
-    //     item.address = this.wallet.convertAddress('legacy', 'cashaddr', item.address)
-    //   })
-    //   this.unspentAddrs.forEach((item: any) => {
-    //     item.address = this.wallet.convertAddress('legacy', 'cashaddr', item.address)
-    //   })
-    // }
     await loader.dismiss()
   }
 
