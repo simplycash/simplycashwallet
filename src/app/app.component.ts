0033df5ab03ed5e55d7ffa6f162d32d8ecdb4ef3
unknown <simplycashdev@gmail.com>
2019-01-07 04:09:13 +0800

    remove unused codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ import { Wallet } from '../providers/providers'
   (window as any).handleOpenURL_LastURL = url
 }
 
-if (window.location.hash === '#/recover') {
-  (window as any).recoveryMode = true
-}
+// if (window.location.hash === '#/recover') {
+//   (window as any).recoveryMode = true
+// }
 
 window.location.hash = ''
 
@@ -71,25 +71,20 @@ export class MyApp {
       } catch (err) {
         console.log(err)
       }
-      if (window.hasOwnProperty('recoveryMode')) {
-        delete (window as any).recoveryMode
-        await this.promptForMnemonic()
-      } else {
-        try {
-          await this.wallet.startWallet()
-          await this.navCtrl.setRoot('HomePage')
-          if (this.platform.is('cordova')) {
-            this.splashScreen.hide()
-          }
-        } catch (err) {
-          console.log(err)
-          this.alertCtrl.create({
-            enableBackdropDismiss: false,
-            title: this.translate.instant('ERROR'),
-            message: this.translate.instant('ERR_START_WALLET_FAILED'),
-            buttons: ['ok']
-          }).present()
+      try {
+        await this.wallet.startWallet()
+        await this.navCtrl.setRoot('HomePage')
+        if (this.platform.is('cordova')) {
+          this.splashScreen.hide()
         }
+      } catch (err) {
+        console.log(err)
+        this.alertCtrl.create({
+          enableBackdropDismiss: false,
+          title: this.translate.instant('ERROR'),
+          message: this.translate.instant('ERR_START_WALLET_FAILED'),
+          buttons: ['ok']
+        }).present()
       }
     })
   }
@@ -130,121 +125,4 @@ export class MyApp {
     })
   }
 
-  // copied from more.ts, no cancel, no empty
-  async promptForMnemonic() {
-    let recoverAlert = this.alertCtrl.create({
-      enableBackdropDismiss: false,
-      title: this.translate.instant('RECOVER_WALLET'),
-      message: `${this.translate.instant('RECOVERY_HINT')}<br><br>(${this.translate.instant('DERIVATION_PATH')}: m/44'/145'/0')`,
-      inputs: [{
-        name: 'mnemonic',
-        placeholder: this.translate.instant('RECOVERY_PHRASE')
-      }],
-      buttons: [/*{
-        text: this.translate.instant('CANCEL'),
-        handler: data => {}
-      },*/{
-        text: this.translate.instant('OK'),
-        handler: data => {
-          if (/*!data.mnemonic || */this.mnemonicIsValid(data.mnemonic)) {
-            this.confirmDelete().then(() => {
-              return recoverAlert.dismiss()
-            }).then(() => {
-              this.recover(data.mnemonic)
-            }).catch((err) => {
-              console.log(err)
-            })
-          }
-          return false
-        }
-      }]
-    })
-    await recoverAlert.present()
-  }
-
-  // copied from more.ts
-  confirmDelete() {
-    return new Promise((resolve, reject) => {
-      let confirmDeleteAlert = this.alertCtrl.create({
-        enableBackdropDismiss: true,
-        title: this.translate.instant('WARN_DELETE_WALLET'),
-        buttons: [{
-          role: 'cancel',
-          text: this.translate.instant('CANCEL'),
-          handler: data => {
-            confirmDeleteAlert.dismiss().then(() => {
-              reject(new Error('cancelled'))
-            })
-            return false
-          }
-        },{
-          text: this.translate.instant('OK'),
-          handler: data => {
-            confirmDeleteAlert.dismiss().then(() => {
-              resolve()
-            })
-            return false
-          }
-        }]
-      })
-      confirmDeleteAlert.present()
-    })
-  }
-
-  // copied from more.ts
-  mnemonicIsValid(m: string) {
-    m = m.trim()
-    if (!this.wallet.validateMnemonic(m)) {
-      this.alertCtrl.create({
-        enableBackdropDismiss: false,
-        title: this.translate.instant('ERROR'),
-        message: this.translate.instant('ERR_INVALID_RECOVERY_PHRASE'),
-        buttons: [this.translate.instant('OK')]
-      }).present()
-      return false
-    } else {
-      return true
-    }
-  }
-
-  // copied from more.ts
-  async recover(m: string) {
-    m = m ? m.trim() : undefined
-    let translations: string[]
-    if (m) {
-      translations = ['RECOVERING', 'RECOVER_SUCCESS', 'RECOVER_FAILED']
-    } else {
-      translations = ['CREATING', 'CREATE_SUCCESS', 'CREATE_FAILED']
-    }
-    let error: Error
-    let loader = this.loadingCtrl.create({
-      content: this.translate.instant(translations[0]) + '...'
-    })
-    loader.present().then(() => {
-      return this.wallet.recoverWalletFromMnemonic(m)
-    }).then(() => {
-      return this.app.getRootNav().setRoot('HomePage')
-    }).catch((err: any) => {
-      console.log(err)
-      error = err
-    }).then(() => {
-      return loader.dismiss()
-    }).then(() => {
-      if (!error) {
-        this.alertCtrl.create({
-          enableBackdropDismiss: false,
-          title: this.translate.instant('SUCCESS'),
-          message: this.translate.instant(translations[1]),
-          buttons: [this.translate.instant('OK')]
-        }).present()
-      } else {
-        this.alertCtrl.create({
-          enableBackdropDismiss: false,
-          title: this.translate.instant('ERROR'),
-          message: this.translate.instant(translations[2]),
-          buttons: [this.translate.instant('OK')]
-        }).present()
-      }
-    })
-  }
 }
